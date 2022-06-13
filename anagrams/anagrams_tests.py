5371ad345030bcc5730cbe0d53c98f93e28e1e4d
Tobias Waslowski <tobias.waslowski@thoughtworks.com>
2022-06-13 09:44:33 +0200

    Fix tests

--- anagrams/anagrams_tests.py
+++ anagrams/anagrams_tests.py
@@ -1,5 +1,6 @@
 import unittest
 
+import anagrams
 from word import Word
 
 
@@ -7,22 +8,35 @@ class MyTestCase(unittest.TestCase):
 
     def test_init_word(self):
         word = Word('cat')
-        assert len(word.char_dict) == 3
-        assert word.char_dict['c'] == 1
-        assert word.char_dict['a'] == 1
-        assert word.char_dict['t'] == 1
+        assert len(word.characters) == 3
 
     def test_compare_unequal_words(self):
         cat = Word('cat')
         doggo = Word('doggo')
 
-        assert cat.hash != doggo.hash
+        assert cat.characters != doggo.characters
 
     def test_compare_anagrams(self):
         kinship = Word('kinship')
         pinkish = Word('pinkish')
 
-        assert kinship.hash == pinkish.hash
+        assert kinship.characters == pinkish.characters
+
+    def integration_test(self):
+        kinship = Word('kinship')
+        pinkish = Word('pinkish')
+
+        enlist = Word('enlist')
+        listen = Word('listen')
+        inlets = Word('inlets')
+
+        cat = Word('cat')
+
+        word_list = [kinship, pinkish, enlist, listen, inlets, cat]
+
+        anagram_cluster = anagrams.cluster_words_into_anagrams(word_list)
+        assert len(anagram_cluster) == 2
+        assert len(anagram_cluster.get(('e', 'i', 'l', 'n', 's', 't'))) == 3
 
 
 if __name__ == '__main__':
